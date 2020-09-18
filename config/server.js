e89444bdff213303e198576839356e3c355e37c5
mashaoliang <salen.ma@qq.com>
2020-09-18 17:05:37 +0800

    feat: initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2f6d104946d36d45f56ea239689a242d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e894436-1e7a-42fe-a926-aba673f1416a'
+};
